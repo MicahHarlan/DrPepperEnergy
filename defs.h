4ae8cc11805b0b61e08ca69d9fa79bb8fea12806
MicahHarlan <81989762+MicahHarlan@users.noreply.github.com>
2022-10-03 14:18:33 -0400

    LCFS Scheduler

---
+++ defs.h
@@ -0,0 +1,21 @@
+/*****************************************************************
+*       defs.h - simplified for CPSC405 Lab by Gusty Cooper, University of Mary Washington
+*       adapted from MIT xv6 by Zhiyi Huang, hzy@cs.otago.ac.nz, University of Otago
+********************************************************************/
+
+struct proc;
+
+// proc.c
+int             Exit(int);
+int             Fork(int);
+int             Kill(int);
+void            pinit(void);
+void            procdump(void);
+void            scheduler(void);
+int             userinit(void);
+int             Wait(int);
+int             Sleep(int, int);
+void            Wakeup(int);
+void			set_weight();
+void 	        set_nice(int,int);
+void            set_timeslice();
